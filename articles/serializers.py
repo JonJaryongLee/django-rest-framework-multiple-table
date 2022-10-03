6a15a09089e4f4fc1d5b276ac170e9b96f67a3bc
JonJaryongLee <director9303@gmail.com>
2022-10-03 19:59:16 +0900

    221003

---
+++ articles/serializers.py
@@ -0,0 +1,15 @@
+from .models import Article, Comment
+from rest_framework import serializers
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ('article',)
